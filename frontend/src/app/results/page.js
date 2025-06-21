e0269d6bae987d0bcdf0f2a314d6deb70f740375
TayalAditya <156252600+TayalAditya@users.noreply.github.com>
2025-06-21 22:40:58 +0530

    Update page.js to solve deployment issue

--- frontend/src/app/results/page.js
+++ frontend/src/app/results/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'; // Because of Next.js 15+ 
+
 import { useState, useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
 import Link from 'next/link'
